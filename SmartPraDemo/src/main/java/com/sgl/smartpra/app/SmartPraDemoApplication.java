d964e7905529599ce46bed29ae0e1d93b8179bb6
anilanal2013 <anilanal2013@gmail.com>
2019-12-20 20:24:15 +0530

    Sample demo - gradle

---
+++ SmartPraDemo/src/main/java/com/sgl/smartpra/app/SmartPraDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sgl.smartpra.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPraDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPraDemoApplication.class, args);
+	}
+
+}
---
+++ SmartPraDemo/src/test/java/com/sgl/smartpra/app/SmartPraDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgl.smartpra.app.SmartPraDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPraDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
